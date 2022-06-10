2612c243ef6362917cde5441d84ff8815a07d6c5
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2022-06-11 06:55:12 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+function pause2 (回数: number) {
+    inPause = 1
+    for (let index = 0; index < 回数; index++) {
+        basic.pause(100)
+        if (inPause == 0) {
+            break;
+        }
+    }
+    neopixelLight.showColor(neopixelLight.colors(neopixelLight.Colors.Black))
+}
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 5) {
         chagePlotData()
@@ -15,36 +25,26 @@ function chagePlotData () {
 }
 function pen (action: string) {
     if (action.substr(0, 1) == "u" || action.substr(0, 1) == "U") {
-        pins.servoWritePin(AnalogPin.P15, penUpDigree)
+        pins.servoWritePin(AnalogPin.P9, penUpDigree)
         basic.pause(200)
     } else if (action.substr(0, 1) == "d" || action.substr(0, 1) == "D") {
-        pins.servoWritePin(AnalogPin.P15, penDownDigree)
+        pins.servoWritePin(AnalogPin.P9, penDownDigree)
         basic.pause(200)
     } else if (action.substr(0, 1) == "r" || action.substr(0, 1) == "R") {
-        neopixel.showColor(neopixel.colors(neopixel.Colors.Red))
+        neopixelLight.showColor(neopixelLight.colors(neopixelLight.Colors.Red))
     } else if (action.substr(0, 1) == "y" || action.substr(0, 1) == "Y") {
-        neopixel.showColor(neopixel.colors(neopixel.Colors.Yellow))
+        neopixelLight.showColor(neopixelLight.colors(neopixelLight.Colors.Yellow))
     } else if (action.substr(0, 1) == "g" || action.substr(0, 1) == "G") {
-        neopixel.showColor(neopixel.colors(neopixel.Colors.Green))
+        neopixelLight.showColor(neopixelLight.colors(neopixelLight.Colors.Green))
     } else if (action.substr(0, 3) == "blu" || action.substr(0, 3) == "BLU") {
-        neopixel.showColor(neopixel.colors(neopixel.Colors.Blue))
+        neopixelLight.showColor(neopixelLight.colors(neopixelLight.Colors.Blue))
     } else if (action.substr(0, 3) == "bla" || action.substr(0, 3) == "BLA") {
-        neopixel.showColor(neopixel.colors(neopixel.Colors.White))
+        neopixelLight.showColor(neopixelLight.colors(neopixelLight.Colors.White))
     }
 }
 input.onButtonPressed(Button.A, function () {
     chagePlotData()
 })
-function pause2 (回数: number) {
-    inPause = 1
-    for (let index = 0; index < 回数; index++) {
-        basic.pause(100)
-        if (inPause == 0) {
-            break;
-        }
-    }
-    neopixel.showColor(neopixel.colors(neopixel.Colors.Black))
-}
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     QUEUE.push(serial.readUntil(serial.delimiters(Delimiters.NewLine)))
 })
@@ -142,11 +142,17 @@ serial.redirectToUSB()
 radio.setGroup(66)
 penUpDigree = 20
 penDownDigree = 40
-pins.servoWritePin(AnalogPin.P15, penUpDigree)
-neopixel.initNeopixel(DigitalPin.P0, 4)
-neopixel.showColor(neopixel.colors(neopixel.Colors.Black))
+pins.servoWritePin(AnalogPin.P9, penUpDigree)
+neopixelLight.initNeopixel(DigitalPin.P0, 4)
+neopixelLight.showColor(neopixelLight.colors(neopixelLight.Colors.Black))
 plotterCar.setParameter(8.37, 81)
 plotterCar.Straight(0)
+basic.forever(function () {
+    if (inExcute == 0) {
+        radio.sendString(plotData[0].substr(1, 20))
+        basic.showString(plotData[0].substr(1, 20))
+    }
+})
 basic.forever(function () {
     if (QUEUE.length > 0) {
         COMMAND = QUEUE.removeAt(0)
@@ -170,11 +176,5 @@ basic.forever(function () {
             inExcute += -1
             watchfont.showNumber2(inExcute)
         }
-    }
-})
-basic.forever(function () {
-    if (inExcute == 0) {
-        radio.sendString(plotData[0].substr(1, 20))
-        basic.showString(plotData[0].substr(1, 20))
     }
 })
